4486d1c45305e05fce1f3a2896f46880d70048bf
pangkeyd <pangkeyd@gmail.com>
2017-11-24 15:25:53 +0700

    Revert "upload-server"

--- server/routers/user.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-const router = express.Router()
-const User = require('../controllers/user')
-
-router.get('/', User.getData)
-
-router.post('/', User.signIn)
-
-module.exports = router
